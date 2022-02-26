fa838fd184dbc0346713a2e54f1e154ca78014a6
Michael Medina <michael.cunha97@hotmail.com>
2022-02-26 09:15:18 -0300

    Adding the game MontyHall maden in VueJS

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1>Projeto de Montyhall</h1>
+      <div class="form">
+          <div v-if="!started">
+              <label for="portsAmount">Quantas Portas?</label>
+              <input type="text" id="portsAmount" size="3"
+                v-model.number="portsAmount">
+          </div>
+          <button v-if="!started" @click="started = true">Iniciar</button>
+          <button v-if="started" @click="started = false; portsAmount = 0">Reiniciar</button>
+      </div>
+      <div class="doors" v-if="started">
+          <div v-for="n in portsAmount" :key="n">
+              <Door :number="n" :hasGift="n == selectedPort" />
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Door from './components/Door.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {Door},
+    data(){
+        return{
+            started: false,
+            portsAmount: 3,
+        }
+    },
+    // Usado como uma tratativa de uma propriedade
+    computed:{
+        selectedPort(){
+           return Math.floor(Math.random() * (this.portsAmount - 0)) + 1 
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+    font-family: 'Montserrat', sans-serif;
+    box-sizing: border-box;
 }
+
+body{
+    color: #fff;
+    background: linear-gradient(to right, rgb(21, 153, 87), rgb(21,87, 153));
+}
+
+#app{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+#app h1{
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 60px;
+}
+
+.doors{
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 40px;
+    align-self: stretch;
+    justify-content: space-around;
+}
+
+.form{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.form, .form input, .form button{
+    margin-bottom: 10px;
+    font-size: 2rem;
+}
+
 </style>
